20ca1d36e25c1126122ac4e9d2c43b8b00156b77
Neph <philippe.balleydier@gmail.com>
2024-08-02 16:37:49 +0200

    automatically resize panels depending on screen size

---
+++ Assets/Scripts/ScreenResizer.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ScreenResizer : MonoBehaviour
+{
+    public GameObject MenuPanel;
+    public GameObject GlobalPanel;
+    public GameObject GameEnginePanel;
+    public GameObject MenuItemPanel;
+
+    private (int, int) OldScreenSize;
+
+    void Start()
+    {
+        OldScreenSize = (Screen.width, Screen.height);
+        ResizeUI();
+    }
+    
+    void Update()
+    {
+        if (OldScreenSize != (Screen.width, Screen.height))
+        {
+            ResizeUI();
+            OldScreenSize = (Screen.width, Screen.height);
+        }
+    }
+
+    void ResizeUI()
+    {
+        RectTransform menuPanelRT = MenuPanel.GetComponent<RectTransform>();
+        RectTransform globalPanelRT = GlobalPanel.GetComponent<RectTransform>();
+        RectTransform gameEnginePanelRT = GameEnginePanel.GetComponent<RectTransform>();
+        RectTransform menuItemPanelRT = MenuItemPanel.GetComponent<RectTransform>();
+
+        bool horizontalScreen = Screen.height * (1 - Settings.Global.MenuRatio * Settings.Global.GameEngineRatio) < Screen.width;
+
+        if (horizontalScreen)
+        {
+            float ratio = Screen.height * Settings.Global.MenuRatio / Screen.width;
+
+            menuPanelRT.anchorMin = new Vector2(0f, 0f);
+            menuPanelRT.anchorMax = new Vector2(ratio, 1f);
+            globalPanelRT.anchorMin = new Vector2(ratio, 0f);
+            globalPanelRT.anchorMax = new Vector2(1f, 1f);
+
+            float gameEngineHeight = Screen.height / 2f;
+            float gameEngineWidth = gameEngineHeight * Settings.Global.GameEngineRatio;
+            if (gameEngineWidth > (1 - Settings.Global.MenuRatio) * Screen.width)
+            {
+                gameEngineWidth = (1 - Settings.Global.MenuRatio) * Screen.width;
+                gameEngineHeight = gameEngineWidth * (1 / Settings.Global.GameEngineRatio);
+            }
+
+            float horizontalRatio = (1 - (gameEngineWidth / ((1 - Settings.Global.MenuRatio) * Screen.width))) / 2;
+            float verticalRatio = (1 - (gameEngineHeight / (Screen.height / 2))) / 2;
+
+            gameEnginePanelRT.anchorMin = new Vector2(horizontalRatio, 0.5f + verticalRatio);
+            gameEnginePanelRT.anchorMax = new Vector2(1 - horizontalRatio, 1f - verticalRatio);
+            menuItemPanelRT.anchorMin = new Vector2(0f, 0f);
+            menuItemPanelRT.anchorMax = new Vector2(1f, 0.5f);
+            
+        }
+        else
+        {
+            float ratio = (Screen.width * Settings.Global.MenuRatio) / Screen.height;
+
+            menuPanelRT.anchorMin = new Vector2(0f, 0f);
+            menuPanelRT.anchorMax = new Vector2(1f, ratio);
+            globalPanelRT.anchorMin = new Vector2(0f, ratio);
+            globalPanelRT.anchorMax = new Vector2(1f, 1f);
+
+            float gameEngineWidth = Screen.width;
+            float gameEngineHeight = gameEngineWidth * (1 / Settings.Global.GameEngineRatio);
+
+            float verticalRatio = 1 - (gameEngineHeight / ((1 - ratio) * Screen.height));
+            gameEnginePanelRT.anchorMin = new Vector2(0f, verticalRatio);
+            gameEnginePanelRT.anchorMax = new Vector2(1f, 1f);
+            menuItemPanelRT.anchorMin = new Vector2(0f, 0f);
+            menuItemPanelRT.anchorMax = new Vector2(1f, verticalRatio);
+        }
+    }
+}
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,10 @@
+﻿public static class Settings
+{
+    public static GlobalSettings Global = new();
+
+    public class GlobalSettings
+    {
+        public float MenuRatio = 0.15f;
+        public float GameEngineRatio = 16f / 9f;
+    }
+}

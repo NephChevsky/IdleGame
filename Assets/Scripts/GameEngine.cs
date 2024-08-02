74d637dd2a256e6bd31ab18416bd227ad9dbb4e7
Neph <philippe.balleydier@gmail.com>
2024-08-03 01:42:31 +0200

    add more ennemies per level

--- Assets/Scripts/GameEngine.cs
+++ Assets/Scripts/GameEngine.cs
@@ -92,7 +92,7 @@ public class GameEngine : MonoBehaviour
         Enemies.Clear();
 
         int mapLevel = PlayerPrefs.GetInt("MapLevel", 1);
-        for (int i = 0; i < 1 /*10 + Mathf.CeilToInt(mapLevel / 10)*/; i++)
+        for (int i = 0; i < 10 + Mathf.CeilToInt(mapLevel / 10); i++)
         {
             RemainingEnemies.Add(EnemyGenerator.CreateEnemy(mapLevel));
         }

339515218f4c1938f810cb0e72456ac5636b71a2
Neph <philippe.balleydier@gmail.com>
2024-08-05 21:01:37 +0200

    clamp livingthing's position when moving

--- Assets/Scripts/LivingThing.cs
+++ Assets/Scripts/LivingThing.cs
@@ -13,8 +13,11 @@ public class LivingThing : MonoBehaviour
     public void Move()
     {
         Vector3 direction = this is Player ? Vector3.right : Vector3.left;
+        RectTransform gameEnginePanelRT = transform.parent.GetComponent<RectTransform>();
         float distance = MovementSpeed * Time.fixedDeltaTime * Settings.Time.GameSpeed;
-        transform.localPosition = transform.localPosition + distance * direction;
+        float posX = transform.localPosition.x + distance * direction.x;
+        posX = Mathf.Clamp(posX, gameEnginePanelRT.rect.width * (-0.5f + Settings.Global.GameUIBorderRatio), gameEnginePanelRT.rect.width * (0.5f - Settings.Global.GameUIBorderRatio));
+        transform.localPosition = new Vector3(posX, transform.localPosition.y, transform.localPosition.z);
     }
 
     public bool Attack(LivingThing opponent)
@@ -34,7 +37,7 @@ public class LivingThing : MonoBehaviour
         Vector3 direction = this is Player ? Vector3.right : Vector3.left;
         string opponentTag = this is Player ? "Enemy" : "Player";
         float range = (Range / 2f) * transform.GetComponent<RectTransform>().rect.width / transform.parent.GetComponent<RectTransform>().rect.width;
-        Debug.DrawRay(transform.position, direction * range);
+        //Debug.DrawRay(transform.position, direction * range);
         if (GetComponent<Collider2D>().Raycast(direction, hits, range) != 0)
         {
             if (hits[0].collider.gameObject.CompareTag(opponentTag))
